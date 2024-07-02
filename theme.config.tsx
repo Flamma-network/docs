e238927bdde83c3de300c147c18bc4fb8d9597c1
clubdrop <158494943+Flamma-network@users.noreply.github.com>
2024-07-02 16:08:36 +0800

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,7 +7,14 @@ const config: DocsThemeConfig = {
     display: 'flex',
     alignItems: 'center',
     gap: '4px'
-  }}><img src={'/icon.svg'} width='30px' />Flamma network</div>,
+  }}>
+    <svg width="83" height="64" viewBox="0 0 83 64" xmlns="http://www.w3.org/2000/svg" className='white size-[30px] dark:text-white'>
+      <path fill-rule="evenodd" clip-rule="evenodd"
+        stroke="currentColor"
+        d="M60.8269 0.647217H79.202H82.721L77.5675 12.5815H74.1498H55.7781L60.8269 0.647217ZM27.9401 0.647512L23.722 3.51676L0.186646 59.8545L1.62869 63.1845L25.7213 38.6284H32.918L37.8723 26.9245H30.1623L36.822 12.5818H52.8399L57.8887 0.647512H27.9401ZM35.8562 38.6285L40.8105 26.9246H63.2146H65.7981L61.1342 38.6285H58.2636H35.8562Z" fill="currentColor" />
+    </svg>
+    <p>Flamma network</p>
+  </div>,
   project: {
     link: 'https://github.com/Flamma-network/docs',
   },
